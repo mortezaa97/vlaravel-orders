10f67e8158cbbd442bd4994e551a802032957313
mortezaa <mortezajafari76@gmail.com>
2025-10-05 10:11:22 +0330

    fix:issues

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,12 @@ use Mortezaa97\Orders\Http\Controllers\SendTypeController;
 
 Route::prefix('api')->middleware('api')->group(function () {
     Route::get('carts', [CartController::class, 'index'])->middleware('auth:api')->name('carts.index');
-    Route::get('carts/{cart}', [CartController::class, 'show'])->name('carts.show');
+    Route::get('carts/{cart:storage_id}', [CartController::class, 'show'])->name('carts.show');
     Route::post('carts', [CartController::class, 'store'])->middleware('auth:api')->name('carts.store');
     Route::post('merge-carts/{cart:storage_id}', MergeCartController::class)->middleware('auth:api')->name('carts.merge');
-    Route::match(['put', 'patch'], 'carts/{cart}', [CartController::class, 'update'])->name('carts.update');
-    Route::delete('carts/{cart}', [CartController::class, 'destroy'])->name('carts.destroy');
-    Route::post('pay/carts/{cart}', PayCartController::class)->middleware('auth:api')->name('carts.pay');
+    Route::match(['put', 'patch'], 'carts/{cart:storage_id}', [CartController::class, 'update'])->name('carts.update');
+    Route::delete('carts/{cart:storage_id}', [CartController::class, 'destroy'])->name('carts.destroy');
+    Route::post('pay/carts/{cart:storage_id}', PayCartController::class)->middleware('auth:api')->name('carts.pay');
 
     Route::get('orders', [OrderController::class, 'index'])->middleware('auth:api')->name('orders.index');
     Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
