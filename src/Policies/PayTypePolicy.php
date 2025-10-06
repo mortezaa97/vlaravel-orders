122e0734e8eb2722bfd58f0e6aa94c0d63fb9f51
mortezaa <mortezajafari76@gmail.com>
2025-10-06 09:49:46 +0330

    fix:issues

--- src/Policies/PayTypePolicy.php
+++ src/Policies/PayTypePolicy.php
@@ -12,7 +12,7 @@ class PayTypePolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(?User $user): bool
     {
         return true;
     }
--- src/Policies/SendTypePolicy.php
+++ src/Policies/SendTypePolicy.php
@@ -12,7 +12,7 @@ class SendTypePolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(?User $user): bool
     {
         return true;
     }
