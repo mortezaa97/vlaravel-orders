186be6ac6e08767d712f33762b803798ee31e9a6
mortezaa <mortezajafari76@gmail.com>
2026-05-20 00:48:55 +0330

    fix: issues

--- src/OrdersPlugin.php
+++ src/OrdersPlugin.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Mortezaa97\Orders;
 
+use App\Enums\Status;
 use Filament\Contracts\Plugin;
+use Filament\Navigation\NavigationItem;
 use Filament\Panel;
 use Mortezaa97\Orders\Filament\Resources\Carts\CartResource;
 use Mortezaa97\Orders\Filament\Resources\Orders\OrderResource;
 use Mortezaa97\Orders\Filament\Resources\PayTypes\PayTypeResource;
 use Mortezaa97\Orders\Filament\Resources\SendTypes\SendTypeResource;
+use Mortezaa97\Orders\Models\Order;
 
 class OrdersPlugin implements Plugin
 {
@@ -28,9 +31,24 @@ class OrdersPlugin implements Plugin
         $panel
             ->resources([
                 'CartResource' => CartResource::class,
-                'OrderResource' => OrderResource::class,
                 'PayTypeResource' => PayTypeResource::class,
                 'SendTypeResource' => SendTypeResource::class,
+                'OrderResource'  => OrderResource::class
+            ])->navigationItems([
+                NavigationItem::make('بررسی نشده ها')
+                    ->url(fn () => OrderResource::getUrl('index', ['filters[status][value]' => Status::PENDING]))
+                    ->icon('heroicon-o-rectangle-stack')
+                    ->group('امور سفارشات')
+                    ->sort(1)
+                    ->isActiveWhen(fn () => request()->fullUrlIs(OrderResource::getUrl('index', ['filters[status][value]' => Status::PENDING], true) . '*')),
+
+                NavigationItem::make('تکمیل شده ها')
+                    ->url(fn () => OrderResource::getUrl('index', ['filters[status][value]' => Status::COMPLETED]))
+                    ->icon('heroicon-o-rectangle-stack')
+                    ->group('امور سفارشات')
+                    ->sort(1)
+                    ->isActiveWhen(fn () => request()->fullUrlIs(OrderResource::getUrl('index', ['filters[status][value]' => Status::COMPLETED], true) . '*')),
+
             ]);
     }
 
